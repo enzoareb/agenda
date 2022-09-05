a93b164fcb1bbc074bd757e9451e726ac87686da
jluisquino <jluisquino@gmail.com>
2022-09-04 23:08:19 -0300

    Fix LocalidadProvinciaDAOSQL

--- agenda/src/main/java/persistencia/dao/mysql/LocalidadProvinciaDAOSQL.java
+++ agenda/src/main/java/persistencia/dao/mysql/LocalidadProvinciaDAOSQL.java
@@ -1,14 +1,11 @@
 package persistencia.dao.mysql;
 
-import java.sql.Connection;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import dto.LocalidadDTO;
 import dto.LocalidadProvinciaDTO;
 import persistencia.conexion.Conexion;
 
